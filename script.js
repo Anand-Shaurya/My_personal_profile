66ba1bd1f79f8cde0b38d7a2834ff496736bc312
Anand Shaurya <shaurya252anand@gmail.com>
2024-07-27 15:42:59 +0530

    updated menu items

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// JavaScript for any interactivity you want to add
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    if (menu.style.display === 'flex') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'flex';
+    }
+}
