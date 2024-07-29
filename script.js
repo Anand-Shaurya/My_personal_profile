254da678ff45aa76deb4e76cca65605bd529df22
Anand Shaurya <shaurya252anand@gmail.com>
2024-07-29 23:25:38 +0530

    Reformatting

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//This function will be used for collapsing and expanding menu.
+
 function toggleMenu() {
     const menu = document.getElementById('menu');
     if (menu.style.display === 'flex') {
